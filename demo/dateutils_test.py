c9792c76e303d2aa3a599445b7dbb4931c2b3095
r350178982 <32759763+r350178982@users.noreply.github.com>
2021-10-18 09:48:54 +0800

    Update dateutils_test.py

--- demo/dateutils_test.py
+++ demo/dateutils_test.py
@@ -82,4 +82,4 @@ print(dateutils.isoweeknum('2012-1-2')) # 1, monday
 print(dateutils.weeknum('2012-1-2')) # 2, monday
 
 # 19. dateutils.isomonth
-print(dateutils.isomonth('2012-10-21 10:20:3'))
+print(dateutils.isomonth('2012-1-2'))
