3d1cd83805c107e5015a27232b703d74007675bb
AlexCXC <1223408988@qq.com>
2023-07-10 11:49:25 +0800

    context add id_in_org

--- seatable_api/context.py
+++ seatable_api/context.py
@@ -53,6 +53,11 @@ class Context:
     def current_username(self):
         return context_data.get('current_username')
 
+    @property
+    @need_data
+    def id_in_org(self):
+        return context_data.get('id_in_org')
+
     @need_data
     def get_setting_by_key(self, key):
         return context_data.get(key)
